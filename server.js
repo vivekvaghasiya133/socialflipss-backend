8737e6bb08edf265b79ac6bc6f21086d78cfadd1
Vivek Vaghasiya <your-email@gmail.com>
2026-04-09 10:15:29 +0530

    removed node_modules from repo

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "http://127.0.0.1:3000"
+  "https://socialflipsswork.vercel.app"
 ];
 
 app.use(cors({
